00aa0cce6704e67aff40c85b835ef68c5499dcf5
bugger69 <aatreya21@gmail.com>
2022-06-02 11:05:24 +0530

    Setting max

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let p1 = 0;
 let p2 = 0;
-let max = 0;
+let max = 1;
 let dropdown = document.querySelector('#dropDown');
 for(let i = 0; i != 10; i++) {
     let option = document.createElement('option');
     option.innerText = `${i + 1}`;
     dropdown.append(option);
 }
+
+dropdown.addEventListener('input', (e) => {
+    max = dropdown.value;
+});
